719234129d0b6de5873188885cec0b7ec8515d2e
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-12-06 17:14:02 +0100

    inserimento file

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+import AppJumbo from '.AppJumbo.vue';
+import MainContent from '.MainContent.vue';
+import AddedContent from './AddedContent.vue';
+import AppFooter from '.AppFooter.vue';
+export default {
+  name: 'AppMain',
+  components:{
+    AppJumbo,
+    MainContent,
+    AddedContent,
+    AppFooter
+  }
+}
+</script>
